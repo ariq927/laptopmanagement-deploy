014b8b3ec9a576a22df62ff9630603c84ec04c7f
Ariq Adlan Hadiyan <ariqhadiyan25@gmail.com>
2025-10-16 16:31:33 +0700

    Force HTTPS for assets

--- resources/views/layouts/blankLayout.blade.php
+++ resources/views/layouts/blankLayout.blade.php
@@ -12,5 +12,20 @@
 
     @vite(['resources/assets/vendor/js/bootstrap.js'])
     @yield('page-script')
+
+    {{-- ===== Production CSS ===== --}}
+  <link rel="stylesheet" href="{{ asset('build/assets/core-BnqA3ef1.css') }}">
+  <link rel="stylesheet" href="{{ asset('build/assets/theme-default-D5KFm6jZ.css') }}">
+  <link rel="stylesheet" href="{{ asset('build/assets/_theme-Ruhj2bpQ.css') }}">
+  <link rel="stylesheet" href="{{ asset('build/assets/custom-override-B_FnKQlU.css') }}">
+  <link rel="stylesheet" href="{{ asset('build/assets/demo-ISkCbL8g.css') }}">
+
+  {{-- ===== Production JS ===== --}}
+  <script src="{{ asset('build/assets/_commonjsHelpers-Cpj98o6Y.js') }}"></script>
+  <script src="{{ asset('build/assets/popper-CgINJS0r.js') }}"></script>
+  <script src="{{ asset('build/assets/bootstrap-BqFZZLXP.js') }}"></script>
+  <script src="{{ asset('build/assets/perfect-scrollbar-DPYX2UL_.js') }}"></script>
+  <script src="{{ asset('build/assets/menu-Bldkajpn.js') }}"></script>
+  <script src="{{ asset('build/assets/main-CWila6Zz.js') }}"></script>
   </body>
 </html>
